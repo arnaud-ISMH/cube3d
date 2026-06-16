80459e251178d7fd2b7b935bfca11882858d0c2a
adeflers <adeflers@f1r3s19.paris.42.school>
2026-06-16 14:30:54 +0200

    norme .h

--- includes/cube3d.h
+++ includes/cube3d.h
@@ -22,96 +22,123 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
-#include <sys/time.h>
+# include <sys/time.h>
 
-int			close_win(t_mlx_data *data);
-int			redraw(t_mlx_data *data);
-t_mlx_data	minilibx_process(void);
-long long	get_time_in_ms(void);
-void		display_score(t_mlx_data *data);
-
-int			put_pixel(t_image *img, int x, int y, unsigned int color);
-int			key_press(int keysym, t_mlx_data *data);
-int			key_release(int keysym, t_mlx_data *data);
-
-void		draw_player(t_mlx_data *data, unsigned int color);
-void		init_player(t_mlx_data *data);
-void		update_player_position(t_mlx_data *data);
-int			mouse_move(int x, int y, t_mlx_data *data);
-
-int			init_monster(t_mlx_data *data);
-void		draw_monster(t_mlx_data *data, unsigned int color);
-void		draw_monster_stripe(t_mlx_data *data, t_monster *monster, t_render_monster m);
-void		render_single_monster(t_mlx_data *data, t_monster *monster);
-void		rsm2(t_mlx_data *data, t_monster *monster, t_render_monster m);
-int			init_monster_tex(t_mlx_data *data);
-void		update_monster_position(t_mlx_data *data);
-void		animate_and_render_monsters(t_mlx_data *data);
-void		sort_monsters(t_mlx_data *data);
-
+// DRAW MAP
 void		draw_map(t_mlx_data *data);
-void		minimap_door(t_mlx_data *data, int x, int y, int open);
-void		minimap_wall(t_mlx_data *data, int x, int y);
-void		minimap_floor(t_mlx_data *data, int x, int y);
 int			init_map(t_mlx_data *data);
 int			is_wall(t_mlx_data *data, double x, double y);
-void		draw_floor_ceiling(t_mlx_data *data, unsigned int f_color, unsigned int c_color);
+
+// INIT
+int			init_textures(t_mlx_data *data);
+int			load_texture(t_mlx_data *data, t_texture *texture, char *path);
+void		display_score(t_mlx_data *data);
+int			redraw(t_mlx_data *data);
+t_mlx_data	minilibx_process(void);
+
+// MINIMAP
+void		minimap_wall(t_mlx_data *data, int x, int y);
+void		minimap_floor(t_mlx_data *data, int x, int y);
+void		minimap_door(t_mlx_data *data, int x, int y, int open);
+void		draw_floor_ceiling(t_mlx_data *data, unsigned int f_color,
+				unsigned int c_color);
 void		draw_minimap_borders(t_mlx_data *data);
 
+// MONSTER
+int			init_monster(t_mlx_data *data);
+void		draw_monster(t_mlx_data *data, unsigned int color);
+void		calculate_monsters_distance(t_mlx_data *data);
+void		sort_monsters(t_mlx_data *data);
+void		draw_monster_stripe(t_mlx_data *data, t_monster *monster,
+				t_render_monster m);
+
+// MONSTER2
+void		animate_and_render_monsters(t_mlx_data *data);
+void		render_single_monster(t_mlx_data *data, t_monster *monster);
+void		rsm2(t_mlx_data *data, t_monster *monster, t_render_monster m);
+void		find_next_xy(t_mlx_data *data, double *next_x, double *next_y,
+				int i);
+void		update_monster_position(t_mlx_data *data);
+
+// PLAYER
+void		init_player(t_mlx_data *data);
+void		draw_player(t_mlx_data *data, unsigned int color);
+void		find_nxy(t_mlx_data *data, double *next_x, double *next_y);
+void		change_oritentation(t_mlx_data *data);
+void		update_player_position(t_mlx_data *data);
+
+// RAYCASTING
+void		choose_tex(t_mlx_data *data, t_raycast *ray);
+void		draw_tex_on_wall(t_mlx_data *data, t_raycast *ray, int i);
+void		render_3d(t_mlx_data *data, t_raycast *ray, int i);
 void		raycasting(t_mlx_data *data);
+
+// RAYCASTING2
 void		get_ray_vect(t_mlx_data *data, t_raycast *ray, int i);
+void		draw_rays_minimap(t_mlx_data *data, t_raycast ray);
 void		init_dda(t_mlx_data *data, t_raycast *ray);
 void		dda_loop(t_mlx_data *data, t_raycast *ray);
 void		fish_eye_correction(t_raycast *ray);
-void		render_3d(t_mlx_data * data, t_raycast *ray, int i);
-void		draw_rays_minimap(t_mlx_data *data, t_raycast ray);
-void		choose_tex(t_mlx_data *data, t_raycast *ray);
-void		draw_tex_on_wall(t_mlx_data *data, t_raycast *ray, int i);
 
-int			init_textures(t_mlx_data *data);
-int			load_texture(t_mlx_data *data, t_texture *texture, char *path);
-
-
+// UTILS
+int			put_pixel(t_image *img, int x, int y, unsigned int color);
 void		trigger_eat_effect(t_mlx_data *data);
 void		eat_animation(t_mlx_data *data);
 void		draw_mm_range_atk(t_mlx_data *data);
 int			eat_monster(t_mlx_data *data);
 
+// UTILS2
+void		free_monster_tex(t_mlx_data *data);
+int			close_win(t_mlx_data *data);
+int			mouse_move(int x, int y, t_mlx_data *data);
 int			open_door(t_mlx_data *data);
 int			detect_closed_door(t_mlx_data *data, t_raycast *ray);
 
+// UTILS3
+void		moove_monster(t_mlx_data *data);
+int			key_press(int keysym, t_mlx_data *data);
+int			key_release(int keysym, t_mlx_data *data);
+long long	get_time_in_ms(void);
+
 /*PARSING*/
 int			read_file(char *arg, t_parsing *parsing);
-int         is_identifier_free(t_parsing *parsing, char c);
+int			is_identifier_free(t_parsing *parsing, char c);
+
 /*CHECKCUB*/
 int			check_color(t_parsing *parsing, char *line, char color);
 int			check_texture(t_parsing *parsing, char *line, char texture);
 int			check_map(t_parsing *parsing, int fd, char *line);
+
 /*GRID*/
 void		fill_map(t_parsing *parsing, t_list *map_list);
 int			flood_fill(t_map *map, int x, int y);
+
 /*MAP*/
 int			ft_forbidden(char c);
-int         parse_map(t_parsing *parsing);
-int	        set_player(t_parsing *parsing, int x, int y, char c);
-int         parse_chara(t_parsing *parsing, int x, int y);
+int			parse_map(t_parsing *parsing);
+int			set_player(t_parsing *parsing, int x, int y, char c);
+int			parse_chara(t_parsing *parsing, int x, int y);
+
 /*UTILS*/
 int			max(int a, int b);
 void		printerr(char *msg);
-void	    free_parsing(t_parsing *parsing);
-char	    **freefree(char **split);
-int         open_fd(char *arg);
+void		free_parsing(t_parsing *parsing);
+char		**freefree(char **split);
+int			open_fd(char *arg);
+
 /*UTILS2*/
-void	    strip(char *s);
-int         ft_isnumber(char *line);
-int         good_color(char *line);
-int         rgb2hex(int r, int g, int b);
+void		strip(char *s);
+int			ft_isnumber(char *line);
+int			good_color(char *line);
+int			rgb2hex(int r, int g, int b);
+
 /*INIT*/
 t_parsing	*init_parsing(void);
-t_parsing   *check_args(int ac, char **av);
+t_parsing	*check_args(int ac, char **av);
+
 /*BONUS*/
-void	count_entities(t_parsing *parsing, int *ndoors, int *nmonsters);
-int     check_door(t_parsing *parsing, int x, int y);
-int     texture_monster(t_parsing *parsing, char *line, char texture);
+void		count_entities(t_parsing *parsing, int *ndoors, int *nmonsters);
+int			check_door(t_parsing *parsing, int x, int y);
+int			texture_monster(t_parsing *parsing, char *line, char texture);
 
 #endif
--- includes/structs.h
+++ includes/structs.h
@@ -6,7 +6,7 @@
 /*   By: lchapot <lchapot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 23:32:17 by adeflers          #+#    #+#             */
-/*   Updated: 2026/06/11 13:50:57 by lchapot          ###   ########.fr       */
+/*   Updated: 2026/06/16 14:02:18 by adeflers         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,31 +27,31 @@ typedef struct s_map
 	int		width;
 	int		height;
 	char	**grid;
-	int scale;
-}		t_map;
+	int		scale;
+}	t_map;
 
-typedef struct	s_color
+typedef struct s_color
 {
-	int r;
-	int g;
-	int b;
+	int	r;
+	int	g;
+	int	b;
 }	t_color;
 
 typedef struct s_keys
 {
-	bool w;
-	bool a;
-	bool s;
-	bool d;
-	bool left;
-	bool right;
-}   t_keys;
+	bool	w;
+	bool	a;
+	bool	s;
+	bool	d;
+	bool	left;
+	bool	right;
+}	t_keys;
 
 typedef struct s_coord
 {
 	double	x;
 	double	y;
-}		t_coord;
+}	t_coord;
 
 typedef struct s_texture
 {
@@ -62,14 +62,14 @@ typedef struct s_texture
 	int		bpp;
 	int		size_line;
 	int		endian;
-}        t_texture;
+}	t_texture;
 
 typedef struct s_door
 {
-    int     x;
-    int     y;
-    int     open;  // 0 fermée, 1 ouverte
-}   t_door;
+	int		x;
+	int		y;
+	int		open;
+}	t_door;
 
 typedef struct s_monster
 {
@@ -79,11 +79,11 @@ typedef struct s_monster
 	t_keys		keys;
 	t_texture	texture[2];
 	t_texture	*current_tex;
-	double		distance; // pour le tri des monstres du plus loin au plus proche du player
+	double		distance;
 	bool		is_alive;
-}		t_monster;
+}	t_monster;
 
-typedef struct	s_parsing
+typedef struct s_parsing
 {
 	char		*no;
 	char		*so;
@@ -93,9 +93,9 @@ typedef struct	s_parsing
 	int			c;
 	char		*t1;
 	char		*t2;
-	int 		player_x;
-	int 		player_y;
-	char 		player_orientation;
+	int			player_x;
+	int			player_y;
+	char		player_orientation;
 	t_map		map;
 	t_door		*door;
 	int			door_count;
@@ -110,7 +110,7 @@ typedef struct s_image
 	int		bpp;
 	int		size_line;
 	int		endian;
-}		t_image;
+}	t_image;
 
 typedef struct s_player
 {
@@ -118,7 +118,7 @@ typedef struct s_player
 	float	dir;
 	double	move_speed;
 	t_keys	keys;
-}		t_player;
+}	t_player;
 
 typedef struct s_raycast
 {
@@ -133,43 +133,43 @@ typedef struct s_raycast
 	double			side_dist_y;
 	int				step_x;
 	int				step_y;
-	double			perp_wall_dist; // Distance perpendiculaire pour éviter l'effet fish-eye
+	double			perp_wall_dist;
 	float			wall_height;
 	int				draw_start;
 	int				draw_end;
-	t_texture		*tex; // Pointeur vers la texture à utiliser pour ce mur
-	double			wall_x; // Position exacte de l'impact sur le mur (0.0 à 1.0)
-	int				tex_x; // Coordonnée X sur la texture
-	int				tex_y; // Coordonnée Y sur la texture
-	double			tex_step; // Combien de pixels de texture on avance pour 1 pixel écran
-	double			tex_pos; // Position de départ de la texture pour l'interpolation verticale
-	int				tex_offset; // offset memoire pour piocher la bonne couleur dans la texture
-	int			 	screen_x; // Coordonnée X à l'écran pour dessiner la colonne verticale
-	int				y_pixel; // Coordonnée Y actuelle à l'écran pour dessiner la colonne verticale
-	unsigned int    color; // Couleur du pixel à dessiner (après avoir pioché dans la texture)
+	t_texture		*tex;
+	double			wall_x;
+	int				tex_x;
+	int				tex_y;
+	double			tex_step;
+	double			tex_pos;
+	int				tex_offset;
+	int				screen_x;
+	int				y_pixel;
+	unsigned int	color;
 	int				hit_door;
-}        t_raycast;
+}	t_raycast;
 
 typedef struct s_render_monster
 {
-    double	sprite_x;
-    double	sprite_y;
-    double	dir_x;
-    double	dir_y;
-    double	plane_x;
-    double	plane_y;
-    double	inv_det;
-    double	transform_x;
-    double	transform_y;
-    int		sprite_screen_x;
-    float	sprite_dim;
-    int		draw_start_y;
-    int		draw_end_y;
-    int		draw_start_x;
-    int		draw_end_x;
-    int		stripe;
+	double	sprite_x;
+	double	sprite_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	double	inv_det;
+	double	transform_x;
+	double	transform_y;
+	int		sprite_screen_x;
+	float	sprite_dim;
+	int		draw_start_y;
+	int		draw_end_y;
+	int		draw_start_x;
+	int		draw_end_x;
+	int		stripe;
 	int		tex_x;
-}		t_render_monster;
+}	t_render_monster;
 
 typedef struct s_mlx_data
 {
@@ -187,11 +187,10 @@ typedef struct s_mlx_data
 	t_texture	texture_south;
 	t_texture	texture_west;
 	t_texture	texture_east;
-	// t_texture	texture_monster;
 	t_texture	texture_door;
 	long long	flash_start_time;
 	bool		is_flashing;
 	int			score;
-}		t_mlx_data;
+}	t_mlx_data;
 
 #endif
